e455c9955c2efc0cc0d1a4e1838328462784123e
maker-kiku <siook1239@gmail.com>
2023-10-25 04:22:30 +0900

    최신화 시간 볼 수 있는 코드 추가

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const jsonFilePath = "docs/data.json"; // JSON 파일의 경로
 const time = [] // 예보시각
 const tmp = [] // 온도
 const pop = [] // 강수확률
+const update_time = null; // 최신화 날짜
 
 // JSON 데이터를 가져오는 함수
 function fetchJSONFile(filePath, callback) {
@@ -17,6 +18,7 @@ function fetchJSONFile(filePath, callback) {
 
 function displayForecast() {
     const forecastResults = document.getElementById("forecast-results");
+    const updateTime = document.getElementById("update-time");
 
     forecastResults.innerHTML += '==================' + '<br>';
     forecastResults.innerHTML += '시간 | 기온 | 강수확률' + '<br>';
@@ -25,10 +27,11 @@ function displayForecast() {
         const result = time[i][0] + time[i][1] + '시 | ' + tmp[i] + '도 | ' + pop[i] + '%';
         forecastResults.innerHTML += result + '<br>';
     }
+    forecastResults.innerHTML += '==================' + '<br>';
 }
 
-// JSON 파일 parse해서 배열에 저장
-fetchJSONFile(jsonFilePath, function(data) {
+// docs/data.json 파일 parse해서 배열에 저장
+fetchJSONFile("docs/data.json", function(data) {
     const jsonData = JSON.stringify(data, null, 2);
     const json_parse = JSON.parse(jsonData);
     const item = json_parse.response.body.items.item;
@@ -47,4 +50,13 @@ fetchJSONFile(jsonFilePath, function(data) {
     }
     
     displayForecast();
+});
+
+// docs/timestamp.json 읽어서 최신화 시간 출력
+fetchJSONFile("docs/timestamp.json", function(data) {
+    const updateTime = document.getElementById("update-time");
+    const jsonData = JSON.stringify(data, null, 2);
+    const json_parse = JSON.parse(jsonData);
+
+    updateTime.innerHTML += '[최신화 시간 : ' + json_parse.timestamp + ']';
 });
